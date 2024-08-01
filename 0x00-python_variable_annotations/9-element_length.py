f9720a698d1a6c088d229f7a29b8838b1637437f
djshampz904 <martin.kimani@strathmore.edu>
2024-08-01 19:49:33 +0300

    another one

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function to_kv that takes a string k and an int OR float v as"""
+from typing import List, Tuple
 
 
-def element_length(lst):
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """Returns a list of tuples, each containing an element and its length"""
     return [(i, len(i)) for i in lst]
