2ac109420c732e0e6fc190301447a14584e2cada
djshampz904 <martin.kimani@strathmore.edu>
2024-08-01 18:15:03 +0300

    another one

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sum of a list of floats module"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return sum of list of floats"""
+    return sum(input_list)
