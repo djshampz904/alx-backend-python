2f3e36dbe59354732eea59b05aa481e8a72532d0
djshampz904 <martin.kimani@strathmore.edu>
2024-08-01 19:43:18 +0300

    another one

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv that takes a string k and an int OR float v as"""
+
+
+def element_length(lst):
+    """Returns a list of tuples, each containing an element and its length"""
+    return [(i, len(i)) for i in lst]
