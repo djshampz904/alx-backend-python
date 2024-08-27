f98fcd3b1b82d4d6a00d88000b0c90fe8f152202
djshampz904 <martin.kimani@strathmore.edu>
2024-08-27 13:43:32 +0300

    concurrent coroutines

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -52,6 +52,34 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test that when calling a_property twice, the correct result
+        is returned but a_method is only called once."""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_class = TestClass()
+            mock_method.return_value = 42
+
+            # Call a_property twice
+            result1 = test_class.a_property
+            result2 = test_class.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
