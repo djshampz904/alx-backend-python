ca72dc2be78a0456ee83698ff95e320af1e7a08a
djshampz904 <martin.kimani@strathmore.edu>
2024-08-27 13:54:52 +0300

    concurrent coroutines

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,6 +57,16 @@ class TestGithubOrgClient(unittest.TestCase):
             )
             mock_public_repos_url.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Unit-test GithubOrgClient.has_license."""
+        client = GithubOrgClient("test-org")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
