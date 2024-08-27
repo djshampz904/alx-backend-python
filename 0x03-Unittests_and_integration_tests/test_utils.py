8ba81e65f9c82a29d7c77cbd419c611aeb547c58
djshampz904 <martin.kimani@strathmore.edu>
2024-08-27 13:40:31 +0300

    concurrent coroutines

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that access_nested_map returns the expected result."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict,
+                                         path: tuple,
+                                         expected_exception_msg: str) -> None:
+        """Test that access_nested_map raises KeyError with correct message."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"'{expected_exception_msg}'")
+
 
 if __name__ == '__main__':
     unittest.main()
